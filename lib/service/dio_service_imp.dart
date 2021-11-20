83aa16bc3ffcbacd45019793ba814d73bcb2fbc3
Adriano Cirino <adriano.cirino.da.silva@gmail.com>
2021-11-20 17:01:39 -0300

    Criado estrutura inicial do projeto

---
+++ lib/service/dio_service_imp.dart
@@ -0,0 +1,20 @@
+import 'package:dio/dio.dart';
+import 'package:dio/src/dio.dart';
+
+import 'dio_service.dart';
+
+class DioServiceImp implements DioService {
+  @override
+  Dio getDIo() {
+    return Dio(
+      BaseOptions(
+        baseUrl: 'https://api.themoviedb.org/4/',
+        headers: {
+          'content-type': 'application/json;charset=utf-8',
+          'authorization':
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyNjY3NTZhYTQ3OWNlMWRjNmJjYTEyZDRiZWY5NDI3OSIsInN1YiI6IjYxOTk0OTMwNGY5YTk5MDA0MzcxNjljYiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._7Q6EO1s_QxRyevkZiQCtXnVDM1vA4O54jQKFCMHFEI',
+        },
+      ),
+    );
+  }
+}
